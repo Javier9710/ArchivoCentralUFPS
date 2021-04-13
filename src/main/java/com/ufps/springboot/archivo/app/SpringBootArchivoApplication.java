67c5a824d30270ad0a55f6ae972486e4d33217c8
Javier Moncada <javierandresmd@ufps.edu.co>
2021-04-12 19:13:37 -0500

    Base

---
+++ src/main/java/com/ufps/springboot/archivo/app/SpringBootArchivoApplication.java
@@ -0,0 +1,13 @@
+package com.ufps.springboot.archivo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootArchivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootArchivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufps/springboot/archivo/app/SpringBootArchivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufps.springboot.archivo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootArchivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
